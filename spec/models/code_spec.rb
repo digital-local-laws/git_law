bb7ba9b90947519ad731394db0c068825ee6d3ec
Ari Epstein <aepstein607@gmail.com>
2015-03-23 16:47:18 -0400

    Add specs for uniqueness and file name generation.

--- spec/models/code_spec.rb
+++ spec/models/code_spec.rb
@@ -7,5 +7,15 @@ RSpec.describe Code, type: :model do
       expect( code.save ).to be false
       expect( code.errors[:name].first ).to include "can't be blank"
     end
+    it "should not save a duplicate name" do
+      code = create(:code)
+      code2 = build(:code, name: code.name)
+      expect( code2.save ).to be false
+      expect( code2.errors[:name].first ).to include "has already been taken"
+    end
+    it "should properly format a file_name" do
+      code = create(:code, name: " Tompkins  County Code ")
+      expect( code.file_name ).to eq "tompkins-county-code"
+    end
   end
 end

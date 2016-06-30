357e4f5ee2c9c3b502c4e44a914afa68b981b998
Ari Epstein <aepstein607@gmail.com>
2016-06-30 09:52:48 -0400

    Split tests for proposing and adopting laws into separate features.

--- features/propose_laws.feature
+++ features/propose_laws.feature
@@ -1,7 +1,7 @@
 Feature: Manage local laws
   In order to comply with filing mandates
   As a local official
-  I want to propose, review, and submit local laws
+  I want to prepare proposed local laws for submission
   @javascript
   Scenario Outline: Authorization for user
     Given a proposed law exists
@@ -90,31 +90,3 @@ Feature: Manage local laws
     Given the section has no text
     When I add text to the section
     Then text should be added to the section
-  @javascript
-  Scenario Outline: Adopt a proposed law
-    Given a jurisdiction exists
-    And each law proposed for the jurisdiction <executive> subject to executive review
-    And I proposed a law
-    And I added a structured code:
-      | level | label   | number      | title | text  | optional |
-      | 1     | chapter | arabic      | yes   | no    | no       |
-      | 2     | section | arabic      | yes   | yes   | no       |
-    And I add a section to the chapter in the code
-    And I edit the text of the section
-    And saving has completed
-    And I log out
-    And I log in as adopter for the jurisdiction
-    When I go to adopt the proposed law
-    And I certify the proposed law <executive> <executive_action> executive review
-    And I certify the law is subject to <referendum> referendum
-    And I adopt the proposed law
-    Then I should see the proposed law is adopted
-    Examples:
-      | executive | executive_action | referendum |
-      | is not    | subject to       | no         |
-      | is        | approved by      | no         |
-      | is        | allowed by       | no         |
-      | is        | rejected by      | no         |
-      | is not    | subject to       | mandatory  |
-      | is not    | subject to       | permissive |
-      | is not    | subject to       | petition   |
---
+++ features/step_definitions/adopt_law_steps.rb
@@ -0,0 +1,58 @@
+When(/^I go to adopt the proposed law$/) do
+  visit "/#/proposed-laws/#{@proposed_law.id}"
+  within(:css, 'h2') do
+    find( :xpath, ".//a[contains(.,'Adopt Law')]" ).click
+  end
+  fill_in 'Finally Adopted On', with: Time.zone.today.to_s(:db)
+end
+
+When /^I certify the proposed law is (not subject|approved|rejected|allowed) (?:to|by) executive review$/ do |action|
+  if action != 'not subject'
+    text = case action
+    when 'approved'
+      action
+    when 'rejected'
+      'repassed after disapproval'
+    else
+      'not approved and no longer subject to approval'
+    end
+    find( :xpath, ".//label[starts-with(.,'#{text}')]" ).click
+    fill_in 'Executive Action Date', with: Time.zone.today.to_s(:db)
+  else
+    expect( page ).to have_no_text 'Executive Action Date'
+  end
+end
+
+When /^I certify the law is subject to (no|mandatory|permissive|petition) referendum/ do |referendum|
+  if referendum == 'no'
+    find( :xpath, ".//label[starts-with(.,'Not subject to referendum')]" ).click
+    expect( page ).to have_no_text 'Referendum Type'
+  else
+    find( :xpath, ".//label[starts-with(.,'Subject to referendum')]" ).click
+    type = case referendum
+    when 'mandatory', 'permissive'
+      referendum
+    else
+      'permissive'
+    end
+    find( :xpath, ".//label[starts-with(.,'#{type}')]" ).click
+    if referendum == 'petition'
+      find( :xpath, ".//label[starts-with(.,'Petition received')]" ).click
+    end
+    if referendum == 'permissive'
+      find( :xpath, ".//label[starts-with(.,'No petition received')]" ).click
+    end
+    if referendum != 'permissive'
+      find( :xpath, ".//label[starts-with(.,'general')]" ).click
+    end
+    fill_in 'Referendum Date', with: Time.zone.today.to_s(:db)
+  end
+end
+
+When(/^I adopt the proposed law$/) do
+  click_button "Certify and Submit Adopted Law"
+end
+
+Then(/^I should see the proposed law is adopted$/) do
+  expect( page ).to have_text "Adopted law was submitted."
+end
